5f2bb63f5abe807f1d5562434dba5e65ec02ab98
mahadyussufnur <mahad.nur-yussuf@outlook.de>
2026-01-16 23:31:32 +0100

    all photos name corrected and dt.js added

---
+++ script/db.js
@@ -0,0 +1,74 @@
+let allDishes = [
+    {
+        "name":"Veggie mushroom black burger",
+        "description":"Mixed green, Tomatoes, Edamame",
+        "price":"16",
+        "image":"Vegie_mushroom_black_burger.png",
+    },
+    {
+        "name":"All meat burger",
+        "description":"Beef, Bacon, Dill pickels, Smkoed cheese, Ketchup, BBQ souse",
+        "price":"15",
+        "image":"Meet_burger.png",
+    },
+    {
+        "name":"Beef red burger",
+        "description":"Beef, Cheese, Tomatoes, Lettuce, Onion",
+        "price":"17",
+        "image":"Red_beef_burger.png",
+    },
+    {
+        "name":"Big chicken burger",
+        "description":"Chicken, Cheese, Tomaoes, Lettuce, onion, Bell, Pepper",
+        "price":"16",
+        "image":"Large_chicken_burger.png",
+    },
+    {
+        "name":"Pizza margherita",
+        "description":"Tomato Suace, Mozzarella",
+        "price":"11",
+        "image":"Margarita.png",
+    },
+    {
+        "name":"Pizza Chorizo",
+        "description":"Tomato slice, Mozzarello, Chorizo",
+        "price":"13",
+        "image":"Pizza_Chrizo.png",
+    },
+    {
+        "name":"Funghi",
+        "description":"Red Onion, Olives, Button Mushrooms, Mozzarello",
+        "price":"14",
+        "image":"Pizza_funghi.png",
+    },
+    {
+        "name":"Quatro Formaggi with Chicken",
+        "description":"Chicken, Mozzarello, Gorgonzola, Fontina, Parmigiano Reggiono",
+        "price":"17",
+        "image":"quatro_formaggi.png",
+    },
+    {
+        "name":"Warm beef argula sald",
+        "description":"Beef, Argula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+        "price":"20",
+        "image":"Beef_argula_salad.png",
+    },
+    {
+        "name":"Mini green Salad",
+        "description":"Green salad, cucumber, Carrots, Parslex, Radishes",
+        "price":"18",
+        "image":"Mini_green_salad.png",
+    },
+    {
+        "name":"Green Salad with Sea food",
+        "description":"Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustardlemon dressing with dill",
+        "price":"22",
+        "image":"Green_salad_with_seafood.png",
+    },
+    {
+        "name":"Vegan green salad with tofu",
+        "description":"Green salad, Cherry tomatoes, Cucumber, baby Spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+        "price":"19",
+        "image":"Vegan_salad_tofu.png",
+    },
+]
