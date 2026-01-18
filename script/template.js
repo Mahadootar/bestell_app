7fec7c3d3f1f1c4749ed9a36249edd6c45bc5648
mahadyussufnur <mahad.nur-yussuf@outlook.de>
2026-01-18 20:03:04 +0100

    edited

--- script/template.js
+++ script/template.js
@@ -3,8 +3,10 @@ function dishesTemplate(dishesIndex){
      <div class="dish_img">
       <img class="food_photo" src="/assets/img/Food_img/${allDishes[dishesIndex].image}">
      </div>
+     <div>
      <h3>${allDishes[dishesIndex].name}</h3>
      <p>${allDishes[dishesIndex].description}<p/>
      <p>${allDishes[dishesIndex].price}<p/>
+     </div>
     </div>`
 }
