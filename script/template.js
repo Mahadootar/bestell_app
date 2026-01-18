afe86a13e25eea21c86d321ba2cee38e2944a369
mahadyussufnur <mahad.nur-yussuf@outlook.de>
2026-01-18 20:45:56 +0100

    correcting the path of the fotos img tag

--- script/template.js
+++ script/template.js
@@ -1,7 +1,7 @@
 function dishesTemplate(dishesIndex){
     return `<div class="dishe_element">
      <div class="dish_img">
-      <img class="food_photo" src="/assets/img/Food_img/${allDishes[dishesIndex].image}">
+      <img class="food_photo" src="./assets/img/Food_img/${allDishes[dishesIndex].image}">
      </div>
      <div>
      <h3>${allDishes[dishesIndex].name}</h3>
