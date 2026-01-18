3a24d5ea3bc4d6aafcaa07dc51b914c54591140b
mahadyussufnur <mahad.nur-yussuf@outlook.de>
2026-01-18 18:06:19 +0100

    the separation orange container eidted

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ function renderDishes(){
     let refDishesContainer = document.getElementById('dishes_container');
     refDishesContainer.innerHTML = "";
 
-    for (let dishesIndex = 0; dishesIndex < allDishes.length; dishesIndex++) {
-        refDishesContainer.innerHTML += dishesTemplate(dishesIndex);
-        
+    for (let dishesIndex = 0; dishesIndex < allDishes.length; dishesIndex++) { 
+        if(dishesIndex == 4 || dishesIndex == 8 ){
+            refDishesContainer.innerHTML += `
+            <div class="dishe-title">
+            </div>`
+        }
+        refDishesContainer.innerHTML += dishesTemplate(dishesIndex)
     }
 }
