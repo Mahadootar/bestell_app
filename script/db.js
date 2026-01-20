319b6b169115833b7f2b827daaf79f7c5b37e6dc
mahadyussufnur <mahad.nur-yussuf@outlook.de>
2026-01-20 17:45:31 +0100

    db updated

--- script/db.js
+++ script/db.js
@@ -1,125 +1,97 @@
 let allDishes = [
+ {
+    "catergory":"Burger",
+    "category-image":"img/pfad",
+    "dishes":[
     {
         "name":"Veggie mushroom black burger",
         "description":"Mixed green, Tomatoes, Edamame",
-        "price":"16",
+        "price":16,
         "image":"black_burger.png",
     },
     {
         "name":"All meat burger",
         "description":"Beef, Bacon, Dill pickels, Smkoed cheese, Ketchup, BBQ souse",
-        "price":"15",
+        "price":16,
         "image":"Meet_burger.png",
     },
     {
         "name":"Beef red burger",
         "description":"Beef, Cheese, Tomatoes, Lettuce, Onion",
-        "price":"17",
+        "price":17,
         "image":"Red_beef_burger.png",
     },
     {
         "name":"Big chicken burger",
         "description":"Chicken, Cheese, Tomaoes, Lettuce, onion, Bell, Pepper",
-        "price":"16",
+        "price":16,
         "image":"Large_chicken_burger.png",
-    },
-    {
+    }, 
+    ]
+},
+{
+     "catergory":"pizza",
+    "category-image":"img/pfad",
+    "dishes":[
+     {
         "name":"Pizza margherita",
         "description":"Tomato Suace, Mozzarella",
-        "price":"11",
+        "price":11,
         "image":"Margarita.png",
     },
     {
         "name":"Pizza Chorizo",
         "description":"Tomato slice, Mozzarello, Chorizo",
-        "price":"13",
+        "price":13,
         "image":"Pizza_Chrizo.png",
     },
     {
         "name":"Funghi",
         "description":"Red Onion, Olives, Button Mushrooms, Mozzarello",
-        "price":"14",
+        "price":14,
         "image":"Pizza_funghi.png",
     },
     {
         "name":"Quatro Formaggi with Chicken",
         "description":"Chicken, Mozzarello, Gorgonzola, Fontina, Parmigiano Reggiono",
-        "price":"17",
+        "price":17,
         "image":"quatro_formaggi.png",
     },
-    {
+    ]
+},
+{
+    
+    "catergory":"Burger",
+    "category-image":"img/pfad",
+    "dishes":[
+        {
         "name":"Warm beef argula sald",
         "description":"Beef, Argula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
-        "price":"20",
+        "price":20,
         "image":"Beef_argula_salad.png",
     },
     {
         "name":"Mini green Salad",
         "description":"Green salad, cucumber, Carrots, Parslex, Radishes",
-        "price":"18",
+        "price":18,
         "image":"Mini_green_salad.png",
     },
     {
         "name":"Green Salad with Sea food",
         "description":"Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustardlemon dressing with dill",
-        "price":"22",
+        "price":22,
         "image":"Green_salad_with_seafood.png",
     },
     {
         "name":"Vegan green salad with tofu",
         "description":"Green salad, Cherry tomatoes, Cucumber, baby Spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
-        "price":"19",
+        "price":19,
         "image":"Vegan_salad_tofu.png",
     },
+    ]
+}   
 ]
 
 const shopping_container = [
-    {
-        "name":"Veggie mushroom black burger",
-        "number": 0,
-    },
-    {
-        "name":"All meat burger",
-        "number": 0,
-    },
-    {
-        "name":"Beef red burger",
-        "number": 0,
-    },
-    {
-        "name":"Big chicken burger",
-        "number": 0,
-    },
-    {
-        "name":"Pizza margherita",
-        "number": 0,
-    },
-    {
-        "name":"Pizza Chorizo",
-        "number": 0,
-    },
-    {
-        "name":"Funghi",
-        "number": 0,
-    },
-    {
-        "name":"Quatro Formaggi with Chicken",
-        "number": 0,
-    },
-    {
-        "name":"Warm beef argula sald",
-        "number": 0,
-    },
-    {
-        "name":"Mini green Salad",
-        "number": 0,
-    },
-    {
-        "name":"Green Salad with Sea food",
-        "number": 0,
-    },
-    {
-        "name": "Vegan green salad with tofu",
-        "number": 0,
-    },
+    
 ]
